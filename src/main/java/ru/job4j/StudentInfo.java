66e37321abfd6d9ff580240c409187f27f4cc5f3
lapandrei <lapihin.andrei@yandex.ru>
2024-06-06 15:15:11 +0300

    Разбор первой программы

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Андрей Лапихин");
+        System.out.println("02.01.1981 г.");
+    }
+}
